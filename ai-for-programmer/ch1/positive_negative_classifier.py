707687b687995342a1c6ec0d6d49fb0b11808d5c
maxentroy52 <maxentroy52@gmail.com>
2025-09-19 11:42:37 +0800

    Fix the compile error.

--- ai-for-programmer/ch1/positive_negative_classifier.py
+++ ai-for-programmer/ch1/positive_negative_classifier.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
+import numpy as np
 
 ## Create the NN.(Specify the structure of NN)
 model = Sequential()
@@ -9,14 +10,15 @@ model.add(Dense(units=1, activation='sigmoid'))
 model.compile(loss='mean_squared_error', optimizer='sgd')
 
 ## Train the NN.
-x = [1, 2, 3, 10, 20, -2, -10, -100, -5, -20]
-y = [1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-model.fit(x, y, epochs=1000, batch_size=4)
+# Convert lists to numpy arrays
+x = np.array([1, 2, 3, 10, 20, -2, -10, -100, -5, -20]).reshape(-1, 1)
+y = np.array([1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0])
+model.fit(x, y, epochs=1000, batch_size=4, verbose=0)
 
 ## Predict.
-test_x = [30, 40, -20, -60]
+test_x = np.array([30, 40, -20, -60]).reshape(-1, 1)
 test_y = model.predict(test_x)
 
 ## Print.
 for i in range(0, len(test_x)):
-    print('input {} => predict: {}'.format(test_x[i], test_y[i]))
+    print('input {} => predict: {}'.format(test_x[i][0], test_y[i][0]))
