9073d0c469cd1570ff6efea7305b6c61bf5017c8
maxentroy52 <maxentroy52@gmail.com>
2026-03-26 15:00:26 +0800

    fish-book(ch4): Add more acc test code.

--- deep-learning-from-scratch/ch4/my_training.py
+++ deep-learning-from-scratch/ch4/my_training.py
@@ -6,13 +6,14 @@ from my_common import softmax
 from my_common import sigmoid
 from my_common import cross_entropy_error
 from my_common import numerical_gradient_nd
+from my_common import plot_results
 
 sys.path.append(os.pardir)
 from dataset.mnist import load_mnist
 
 class ImageRecognizerNN():
     ## ---------------------------- Basic method for ImageRecognizer-----------------------------------
-    def __init__(self, input_size, hidden_size, output_size, weight_init_std = 0.01, learning_rate=0.1, iterations=10000, batch_size=100):
+    def __init__(self, input_size, hidden_size, output_size, weight_init_std = 0.01, learning_rate=0.1, iterations=1000, batch_size=100):
         # Model parameters.
         self.input_size = input_size
         self.hidden_size = hidden_size
@@ -79,10 +80,14 @@ class ImageRecognizerNN():
 
         return grads
 
-    def train(self, x_train, t_train):
+    def train(self, x_train, t_train, x_test, t_test):
         print("---------------------Training begins:---------------------")
         loss_list = []
         train_size = x_train.shape[0]
+        iter_per_epoch = max(int(train_size / self.batch_size), 1)
+        train_acc_list = []
+        test_acc_list = []
+
         for iteration in range(self.iterations):
             start_time = time.time()
             print(f"iteration:{iteration} begins.")
@@ -96,7 +101,7 @@ class ImageRecognizerNN():
             # calculate the gradient for the model parameters
             grads = self.gradient_numerical(x_batch, t_batch)
 
-            # update the model parameters based on gradient descent.
+            # update the model parameters based on mini-batch samples.
             self.model['W1'] -= self.learning_rate * grads['W1']
             self.model['b1'] -= self.learning_rate * grads['b1']
             self.model['W2'] -= self.learning_rate * grads['W2']
@@ -111,9 +116,25 @@ class ImageRecognizerNN():
 
             print(f"iteration:{iteration} end, loss is {loss}, elapsed time is {elapsed_time:.2f}s")
 
+            # Calculate recognition accuracy for each epoch
+            # It should be printed in every epoch.
+            if iteration % 10 == 0:
+                train_acc = self.accuracy(x_train, t_train)
+                test_acc = self.accuracy(x_test, t_test)
+                train_acc_list.append(train_acc)
+                test_acc_list.append(test_acc)
+                print(f"train_acc:{train_acc}, test_acc:{test_acc}")
+
+        plot_results(loss_list, train_acc_list, test_acc_list)
+
     ## ---------------------------- Acc calculation -----------------------------------
+    def accuracy(self, x_test, t_test):
+        y_pred = self.predict(x_test)
+        y_pred = y_pred.argmax(axis=1)
+        t_test = t_test.argmax(axis=1)
 
-
+        accuracy = np.sum(y_pred == t_test)
+        return accuracy / float(x_test.shape[0])
 
 def test_image_recognizer():
     image_recognizer = ImageRecognizerNN(784, 100, 10)
@@ -125,6 +146,6 @@ def test_image_recognizer():
     print(x_train.shape, t_train.shape)
     print(x_test.shape, t_test.shape)
 
-    image_recognizer.train(x_train, t_train)
+    image_recognizer.train(x_train, t_train, x_test, t_test)
 
 test_image_recognizer()
