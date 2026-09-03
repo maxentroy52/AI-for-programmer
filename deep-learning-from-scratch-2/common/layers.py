76a68a086a29ca8115e533ec05ac0d1522645b62
maxentroy52 <maxentroy52@gmail.com>
2026-09-03 20:14:50 +0800

    ch1: Update readme.

--- deep-learning-from-scratch-2/common/layers.py
+++ deep-learning-from-scratch-2/common/layers.py
@@ -44,6 +44,21 @@ class Affine:
         self.grads[1][...] = db
         return dx
 
+class Softmax:
+    def __init__(self):
+        self.params, self.grads = [], []
+        self.out = None
+
+    def forward(self, x):
+        self.out = softmax(x)
+        return self.out
+
+    def backward(self, dout):
+        dx = self.out * dout
+        sumdx = np.sum(dx, axis=1, keepdims=True)
+        dx -= self.out * sumdx
+        return dx
+
 class SoftmaxWithLoss:
     def __init__(self):
         self.params, self.grads = [], []
@@ -59,3 +74,38 @@ class SoftmaxWithLoss:
 
         loss = cross_entropy_error(self.y, self.t)
         return loss
+
+class Sigmoid:
+    def __init__(self):
+        self.params, self.grads = [], []
+        self.out = None
+
+    def forward(self, x):
+        out = 1 / (1 + np.exp(-x))
+        self.out = out
+        return out
+
+    def backward(self, dout):
+        dx = dout * (1.0 - self.out) * self.out
+        return dx
+
+class SigmoidWithLoss:
+    def __init__(self):
+        self.params, self.grads = [], []
+        self.loss = None
+        self.y = None  # sigmoidの出力
+        self.t = None  # 教師データ
+
+    def forward(self, x, t):
+        self.t = t
+        self.y = 1 / (1 + np.exp(-x))
+
+        self.loss = cross_entropy_error(np.c_[1 - self.y, self.y], self.t)
+
+        return self.loss
+
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+
+        dx = (self.y - self.t) * dout / batch_size
+        return dx
